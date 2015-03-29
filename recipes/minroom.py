193bcbd311b37d9f33bbac51167daccc6b08ed7f
David Whale <david@thinkingbinaries.com>
2015-03-29 13:47:17 +0100

    First pass of maths for the proximity calculations done. Test harness added, ready for testing.

--- recipes/minroom.py
+++ recipes/minroom.py
@@ -1,83 +1,129 @@
 # minroom.py  21/03/2015  D.J.Whale
 #
-# Model of a minimum room and it's surroundings,
-# lower left front corner translated to (0,0,0).
-# Any room can be scaled down to this minimum room.
-# Any point coordinate can be scaled down to this minimum room.
-# It then allows point comparisons to be made for proximity and face.
-# Room coefficients are (ox,oy,oz,sx,sy,sz)
-# o=offset(subtracted), s=scale(divided by)
+# Model of a minimum room and it's surroundings.
+# The minimum room is a 9x9x9 model as follows:
+# 0 = outside, 1=outside_touching, 2=on, 3=inside_touching
+# 4 = inside
+# 5 = inside_touching, 6=on, 7=outside_touching, 8=outside
 #
-# Assessment of proximity and face is then a 3D lookup of an x,y,z index
-# in this model.
+# Any 3D rectangular room can be scaled down to this minroom.
+# There is an internal 9x9x9 lookup table, where the scaled down
+# coordinates are used to index into this table, and the type of proximity
+# can be quickly calculated.
 #
+# The model is pre-computed at startup.
 # Pre-computing the model makes proximity and face calculations much
 # simpler, and therefore quicker, at runtime.
 # This is important if you are doing regular proximity and face checks,
 # e.g. for geofencing a forcefield protected room while a player is
 # moving live.
+#
+# How to use:
+# 1) work out the translation coefficients that map your room onto minroom
+#    translation = getCubeTranslation(x1, y1, z1, x2, y2, z2)
+#    x1,y1,z1 is lower left corner of wall of room
+#    x2,y2,z2 is upper right corner of wall of room
+#
+# 2) Get the position of a point in or around the room (e.g. player pos)
+#    pos = mc.player.getTilePos()
+#
+# 3) Translate the point and get the proximity type for that position
+#    prox, face = getPointProximity(translation, pos.x, pos.y, pos.z)
+#
+# 4) Make a decision based on proximity type
+#    if prox == Proximity.OUTSIDE:
+#    if prox == Proximity.OUTSIDE_TOUCHING:
+#    if prox == Proximity.ON:
+#    if prox == Proximity.INSIDE_TOUCHING:
+#    if prox == Proximity.INSIDE:
+#
+# 5) Make a decision based on face type
+#    if face == Face.NORTH:
+#    if face == Face.EAST:
+#    if face == Face.SOUTH:
+#    if face == Face.WEST:
+#    if face == Face.FLOOR:
+#    if face == Face.CEILING:
+#    if face == Face.NONE:
 
 
+def getCubeTranslation(x1, y1, z1, x2, y2, z2):
+    """Calculate offsets and scales to translate any room down to minroom"""
+    # cube parameter coordinates
+    # x1,y1,z1 smallest coordinate on the wall
+    # x2,y2,z2 biggest coordinate on the wall
 
-# Perform pre-compression of the point
-# and pre-compute the model as a unity model (i.e. the smallest it can be)
-# point pre-compression could be done by translating the point and the
-# model to a near-zero based origin (removing offset).
-# the scale could be compressed by....some simple numeric modulus scheme?
-# is it a sort of y=mx+c style problem - take off c to translate to zero,
-# divide point coords by some divisor to make it fit the unity model,
-# then just index into the unity model table (3D table) and read back the
-# answer which gives in/touching/out/facetype in one go.
+    #TODO refactor, 3 times the same equation, put in a function
+    
+    # calculate outside coordinates
+    x1_os = x1-2
+    y1_os = y1-2
+    z1_os = z1-2
 
-# Here's how this would work:
-# minimum model is 9x9x9, with tuples of (proximity, face) in each point.
-# work out what divisors in x,y,z are required to scale down the
-# forcefield cuboid to a minimum model size (3 float divisions).
-# remember these divisors as coefficients.
-# work out what subtractors in x,y,z are required to translate the
-# forcefield cuboid onto the minimum model (corner at 0,0,0)
-# remember these 3 integer coefficients.
-# To get the proximity and face of an x,y,z point
-#   subtract offset coefficients
-#   float divide px,py,pz by the scale coefficients
-#   convert all to int
-#   lookup x,y,z in the minimal model
-#   the resultant tuple is the proximitytype+face.
-# the memory space requirements are minimal.
-# the computation requirements are small (3 float conversions,
-# 3 float divisions, 3 integer conversions, 3 integer subtractions,
-# a 3-way index into a data structure).
+    x2_os = x2+2
+    y2_os = y2+2
+    z2_os = z2+2
+    
+    # translate so that lowest non-touching corner is at 0,0,0
+    # translate lowest point down to minroom corner
 
+    ox = x1_os
+    oy = y1_os
+    oz = z1_os
+    
+    # translate highest point by the same amount (not scaled yet)
+    # this puts p2_new outside on far edge
+    x2_new = x2_os - ox
+    y2_new = y2_os - oy
+    z2_new = z2_os - oz
 
+    # work out scale factor to scale p2 down to minroom
+    # minroom is 9x9x9
 
+    sx = float(x2_new) / 9.0
+    sy = float(y2_new) / 9.0
+    sz = float(z2_new) / 9.0
 
-#def translateCube(x1, y1, z1, x2, y2, z2):
-#    #TODO calculate scales and offsets to translate any point in/around room
-#    #down to the model coordinates
-#    return (ox, oy, oz, sx, sy, sz)
-
-
-
-#def translatePoint(room, px, py, pz):
-#    # translate a point using the room coefficients.
-#    # handle out of range (-ves) properly
-#    return ix, iy, iz # TODO indexes into model
+    # To translate any point in user_room to min_room:
+    # x=(x-ox)/sx, y=(y-oy)/sy, z=(z-oz)/sz
+    return (ox, oy, oz, sx, sy, sz)
 
 
-#def getPointProximity(room, px, py, pz):
-#    ix, iy, iz = translatePoint(room, px, py, pz)
-#    proximity, face = model[ix][9-iy][iz]
-#    return proximity, face
+def translatePoint(translation, px, py, pz):
+    """Translate a single point down to the minroom"""
+    ox, oy, oz, sx, sy, sz = translation # unpack translation coefficients
+
+    # apply offset then scale
+    mpx = (px-ox)/sx
+    mpy = (py-oy)/sy
+    mpz = (pz-oz)/sz
+
+    # make into int indexes
+    # TODO round() or trunc() here??
+
+    mpx = int(mpx)
+    mpy = int(mpy)
+    mpz = int(mpz)
+
+    return (mpx, mpy, mpz)
+       
+
+def getPointProximity(translation, px, py, pz):
+    """Work out the proximity type of this point to the given room"""
+    
+    ix, iy, iz = translatePoint(translation, px, py, pz)
+    proximity, face = model[ix][iy][iz]
+    return proximity, face
     
 
 # Proximity types
 class Proximity:
-    UNKNOWN            = 'X'
-    INSIDE             = 'I'
-    INSIDE_TOUCHING    = 'IZ'
-    OUTSIDE            = 'O'
-    OUTSIDE_TOUCHING   = 'OZ'
-    ON                 = 'Z'
+    UNKNOWN            = "unknown"          # 'X'
+    INSIDE             = "inside"           # 'I'
+    INSIDE_TOUCHING    = "inside_touching"  # 'IZ'
+    OUTSIDE            = "outside"          # 'O'
+    OUTSIDE_TOUCHING   = "outside_touching" # 'OZ'
+    ON                 = "on"               #'Z'
  
 # Face types
 class Face:
@@ -140,8 +186,9 @@ def fillProximity(model):
 
 def calcFace(x,y,z):
     return '?'
-    #TODO calculate N,S,E,W,F,C
+    #TODO calculate N,S,E,W,F,C,X
     #based on an algebraic specification with respect to (x,y,z)
+    #with x,y,z == (0..8)
 
 
 def fillFaces(model):                   
@@ -168,7 +215,6 @@ def visualise():
     pos = mc.player.getTilePos()
     pos.x += 1
 
-
     prox_list  = [
         Proximity.INSIDE,
         Proximity.INSIDE_TOUCHING,
@@ -184,7 +230,6 @@ def visualise():
         block.GLASS.id,
         block.STONE.id
     ]
-
 
     for i in range(len(prox_list)):
         wantedProximity = prox_list[i]
@@ -210,12 +255,46 @@ def visualise():
     #when the module restarts.
 
 
+def buildroom(x1, y1, z1, x2, y2, z2, b):
+    # build a physical room
+    mc.setBlocks(x1,   y1,   z1,   x2,   y2,   z2,   b)
+    mc.setBlocks(x1+1, y1+1, z1+1, x2-1, y2-1, z2-1, block.AIR.id)
+
+    
+def testroom():
+    import mcpi.minecraft as minecraft
+    import mcpi.block as block
+    import time
+    
+    R = (0,0,0,10,10,10)
+    
+    buildroom(R[0], R[1], R[2], R[3], R[4], R[5], block.GLASS.id)
+
+    # work out translation coefficients
+    translate = getCubeTranslation(R[0], R[1], R[2], R[3], R[4], R[5])
+
+    try:
+        # game loop - monitor player proximity
+        while True:
+            time.sleep(1)
+            pos = mc.player.getTilePos()
+            prox,face = getProximity(translate, pos.x, pos.y, pos.z)
+            mc.postToChat("prox:" + str(prox))
+    finally:
+        # destroy the room on exit
+        buildroom(R[0], R[1], R[2], R[3], R[4], R[5], block.AIR.id)
+        
+
+# MAIN PROGRAM -----------------------------------------------------------------
+
+model = buildEmptyModel(9, 9, 9)
+fillProximity(model)
+fillFaces(model)
+
 if __name__ == "__main__":
-    model = buildEmptyModel(9,9,9)
-    fillProximity(model)
-    fillFaces(model)
-    print(str(model))
-    visualise()
+    #print(str(model))
+    #visualise()
+    testroom()
 
 # END
 
